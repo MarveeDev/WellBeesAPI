30f847e80d98ab1452dc7f32fb8109d2888574db
MarveeDev <mrvptr06t18d416e@iisbadoni.edu.it>
2024-11-15 19:51:03 +0100

    i

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import serial
 import time
-import threading
 
-# Crea l'istanza dell'app FastAPI
+# Crea l'app FastAPI
 app = FastAPI()
 
-# Configura la connessione seriale con Arduino (modifica con la tua porta)
-arduino = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
 
-# Funzione per inviare valori ad Arduino
-def send_values_to_arduino():
-    """Funzione per inviare un ciclo di valori ad Arduino"""
-    for i in range(10):  # Loop che invia valori da 0 a 9
-        print(f"Inviando valore {i} a Arduino...")
-        arduino.write(f"{i}\n".encode())  # Invia il valore come stringa, seguito da una nuova linea
-        time.sleep(1)  # Attende 1 secondo prima di inviare il prossimo valore
+arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
-    print("Invio valori completato!")
 
-# Endpoint per iniziare l'invio di valori a Arduino
-@app.get("/start-sending-to-arduino")
-async def start_sending():
-    """Endpoint che avvia l'invio dei valori ad Arduino"""
-    # Avvia un thread separato per inviare i valori ad Arduino in parallelo
-    threading.Thread(target=send_values_to_arduino).start()
-    return {"message": "Inizio invio dei valori ad Arduino!"}
-
-# Funzione per leggere la risposta da Arduino (se necessario)
-def read_from_arduino():
-    """Leggi la risposta di Arduino sulla seriale"""
+# Funzione per leggere la distanza dal sensore
+def get_distance_from_arduino():
+    print("Richiesta di lettura della distanza dal sensore ultrasonico 2")
+    arduino.write(b"get_distance\n")
+    time.sleep(0.5)
     if arduino.in_waiting > 0:
-        line = arduino.readline().decode('utf-8').strip()
-        return line
+        print("Dati disponibili")
+        distance = arduino.readline().decode('utf-8').strip()  # Leggi la distanza
+        return distance
+    print("Nessun dato disponibile")
     return None
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/ultrasonic/{id}/distance")
+async def get_ultrasonic_distance(id: int):
+    print("Richiesta di lettura della distanza dal sensore ultrasonico")
+    """Funzione per ottenere la distanza dal sensore ultrasonico collegato ad Arduino."""
+    distance = get_distance_from_arduino()
+
+    if distance is None:
+        raise HTTPException(status_code=500, detail="Errore nella lettura della distanza da Arduino")
+
+    return {"distance": distance}
